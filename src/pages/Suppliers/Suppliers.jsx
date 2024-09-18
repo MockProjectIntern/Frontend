19859586d30de71e2258d882584d43d849ee2276
Thái Nguyễn <thainv215135.vnist@gmail.com>
2024-09-18 23:28:04 +0700

    list supplier page

--- src/pages/Suppliers/Suppliers.jsx
+++ src/pages/Suppliers/Suppliers.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SupplierList from '../../components/SupplierList/SupplierList'
 
 const Suppliers = () => {
   return (
-    <div>Suppliers</div>
+    <>
+      <Routes>
+        <Route path='/' element={<SupplierList />} />
+      </Routes>
+    </>
   )
 }
 
