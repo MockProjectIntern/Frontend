93e46af347ba7a9bfa9ddc2c12a753b853e77189
Nguyen Van <121248948+VanNguyenHust@users.noreply.github.com>
2024-09-25 05:11:13 +0700

    [FIX]: Fix error name file

--- src/pages/ReceiptVouchers/ReceiptVouchers.jsx
+++ src/pages/ReceiptVouchers/ReceiptVouchers.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
 import ReceiptVouchersList from '../../components/ReceiptVouchersList/ReceiptVouchersList'
 import CreateReceiptVoucher from '../../components/CreateReceiptVoucher/CreateReceiptVoucher'
 
