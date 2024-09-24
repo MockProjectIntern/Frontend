c83a9882e12169c891d5556c97c520dfd9f7e665
Nguyen Van <121248948+VanNguyenHust@users.noreply.github.com>
2024-09-24 22:59:50 +0700

    Squashed commit of the following:
    commit e2cf46dc31e1cb0caaa2cf45c4e89bb81ee12fb8
    Merge: b16461b 590be6d
    Author: Tan-Tien-Le <duytan3105@gmail.com>
    Date:   Tue Sep 24 23:00:59 2024 +0700
    
        [FEAT}: intergrate api to gin page
    
    commit b16461b6d536285e293113100631a9f49f48f919
    Merge: 3c118a9 411bfea
    Author: Tan-Tien-Le <duytan3105@gmail.com>
    Date:   Mon Sep 23 13:35:15 2024 +0700
    
        Merge branch 'develop' of https://github.com/MockProjectIntern/Frontend into create-page-for-creating-gin
    
    commit 3c118a9ac90825cd149549f41ff738bea1621ef9
    Merge: 8df6087 149b89a
    Author: Tan-Tien-Le <duytan3105@gmail.com>
    Date:   Sun Sep 22 23:09:17 2024 +0700
    
        Merge branch 'develop' of https://github.com/MockProjectIntern/Frontend into create-page-for-creating-gin
    
    commit 8df6087987765901d6236a6d411cb84cabeb5478
    Author: Tan-Tien-Le <duytan3105@gmail.com>
    Date:   Sat Sep 21 16:24:03 2024 +0700
    
        [FEAT] : Create component for creating gin

---
+++ src/components/GINProductsTable/GINProductsTable.jsx
@@ -0,0 +1,126 @@
+import React, { useState , useRef} from 'react'; // Add useState
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+
+// Import CSS
+import s from './GINProductsTable.module.scss';
+
+// Import Columns Info
+import productsTableColgroup from '../../assets/colgroup/gin-products-table';
+
+// Import Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleMinus, faCirclePlus, faXmark } from '@fortawesome/free-solid-svg-icons';
+import dropdownicon from '../../assets/icons/TriangleDropdown'; // Adjust path as needed
+import ReasonSelectPopup from '../ReasonSelectPopup/ReasonSelectPopup';
+import ReasonTableCell from '../ReasonTableCell/ReasonTableCell';
+
+const GINProductsTable = ({ productsList }) => {
+  
+  return (
+    <div className={s.container}>
+      <table className={s.table}>
+        <colgroup>
+          {/* Render the <colgroup> based on productsTableColgroup */}
+          {Object.entries(productsTableColgroup).map(([key, col]) => (
+            <col key={key} style={{ width: col.width, minWidth: col.minWidth || "unset" }} />
+          ))}
+          <col style={{ width: "40px" }} />
+        </colgroup>
+        <thead className={s.tableHeader}>
+          <tr className={s.tableRow}>
+            {/* Render table headers based on productsTableColgroup */}
+            {Object.entries(productsTableColgroup).map(([key, col]) => (
+              <th key={key} scope='col' className={cn(s.tableCell, s.tableCellHeader, col.align)}>
+                <p>{col.name}</p>
+              </th>
+            ))}
+            <th className={cn(s.tableCell, s.tableCellHeader, "text-center")}></th>
+          </tr>
+        </thead>
+        <tbody className={s.tableBody}>
+          {productsList.map((product, index) => (
+            <tr key={index} className={s.tableRow}>
+              {Object.entries(productsTableColgroup).map(([key, col]) => {
+                if (key === "image") {
+                  return (
+                    <td key={key} className={cn(s.tableCell, s.tableCellBody, col.align)}>
+                      <img src={product.image} alt="" />
+                    </td>
+                  );
+                } else if (key === "name") {
+                  return (
+                    <td key={key} className={cn(s.tableCell, s.tableCellBody, col.align)}>
+                      <div className={s.boxInfo}>
+                        <div className={s.boxName}>
+                          <p>{product.name}</p>
+                        </div>
+                        <div className={s.boxId}>
+                          <Link to={`/admin/products/${product.id}`}>
+                            <p>{product.id}</p>
+                          </Link>
+                        </div>
+                      </div>
+                    </td>
+                  );
+                } else if (key.includes("actual_stock")) {
+                  return (
+                    <td key={key} className={cn(s.tableCell, s.tableCellBody, col.align)}>
+                      <div className={s.boxQuantity}>
+                        <button className={s.btnIcon}>
+                          <span className={s.btnIconLabel}>
+                            <FontAwesomeIcon icon={faCircleMinus} />
+                          </span>
+                        </button>
+                        <div className={s.boxFormControl}>
+                          <div className={s.boxInput}>
+                            <input className={cn(s.inputField, col.align)} type="text" value={product[key]} />
+                          </div>
+                        </div>
+                        <button className={s.btnIcon}>
+                          <span className={s.btnIconLabel}>
+                            <FontAwesomeIcon icon={faCirclePlus} />
+                          </span>
+                        </button>
+                      </div>
+                    </td>
+                  );
+                } else if (key === "reason") {
+                  return (
+                <ReasonTableCell />
+                  );
+                } else if (key === "note") {
+                  return (
+                    <td key={key} className={cn(s.tableCell, s.tableCellBody, col.align)}>
+                      <input className={s.boxNote} type="text" placeholder="Nhập ghi chú" />
+                    </td>
+                  );
+                }
+                return (
+                  <td key={key} className={cn(s.tableCell, s.tableCellBody, col.align)}>
+                    <p>{key === "index" ? index + 1 : product[key]}</p>
+                  </td>
+                );
+              })}
+              <td className={cn(s.tableCell, s.tableCellBody, "text-center")}>
+                <button className="btn-icon">
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {productsList.length === 0 && (
+        <div className={s.boxEmpty}>
+          <p>Bảng chưa có sản phẩm nào</p>
+          <button className="btn btn-outline-primary">
+            <span className="btn__label">Thêm sản phẩm</span>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default GINProductsTable;
