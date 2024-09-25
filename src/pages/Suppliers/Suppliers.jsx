8ac9862198597672b5f9b6340e39f0d198e0cab3
Thái Nguyễn <123443192+iaht211@users.noreply.github.com>
2024-09-25 17:52:10 +0700

    stash

--- src/pages/Suppliers/Suppliers.jsx
+++ src/pages/Suppliers/Suppliers.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SupplierList from '../../components/SupplierList/SupplierList'
+import CreateSupplier from '../../components/CreateSupplier/CreateSupplier'
 
 const Suppliers = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<SupplierList />} />
+        <Route path='/create' element={<CreateSupplier />} />
       </Routes>
     </>
   )
