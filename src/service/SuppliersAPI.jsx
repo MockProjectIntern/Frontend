a52ea77ad79590c36ab8df002ccc3fd4b4a13827
nguyenvanthai092003 <nguyenvanthai092003@gmail.com>
2024-09-26 22:11:32 +0700

    fix bug create supplier api

--- src/service/SuppliersAPI.jsx
+++ src/service/SuppliersAPI.jsx
@@ -2,14 +2,14 @@ import axiosInstance from "./axiosInstance";
 
 export const getSupplierList = async (page, size, filterName, filterJson, bodyJson) => {
     try {
-        const response = await axiosInstance.post(`suppliers/filter.json?page=${page}&size=${size}`, {
+        const response = await axiosInstance.post(`suppliers/filter.json?page=${page}&size=${size}`,
             // Payload của yêu cầu (body)
             bodyJson
-        }, {
-            headers: {
-                [filterName]: filterJson // Thiết lập giá trị của filterName vào header
-            }
-        });
+            , {
+                headers: {
+                    [filterName]: filterJson // Thiết lập giá trị của filterName vào header
+                }
+            });
         return response.data;
     } catch (err) {
         console.log(err);
