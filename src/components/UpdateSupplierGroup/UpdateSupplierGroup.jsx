7b0086f3386e8a7222a8772fef1407a0eaf7a935
Nguyen Van <121248948+VanNguyenHust@users.noreply.github.com>
2024-10-03 17:21:46 +0700

    Squashed commit of the following:
    commit 2a9c961384a8d4bc05a6e5d4df05bdfebd1222d6
    Author: AnTu <an106203h@gmail.com>
    Date:   Thu Oct 3 17:17:29 2024 +0700
    
        xong sua nhom nha cc

---
+++ src/components/UpdateSupplierGroup/UpdateSupplierGroup.jsx
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react";
+import s from "./UpdateSupplierGroup.module.scss";
+import { updateSupplierGroup } from "../../service/SupplierGroupsAPI";
+const UpdateSupplierGroup = ({ handleOnClickBack, item }) => {
+  const [data, setData] = useState({});
+
+  const handleUpdate = async () => {
+    const response = await updateSupplierGroup(data.id, {
+      sub_id: data.sub_id,
+      name: data.name,
+      note: data.note,
+    });
+    if (response.status_code === 200) {
+      alert("Cập nhật nhóm nhà cung cấp thành công");
+      handleOnClickBack();
+    }
+  };
+
+  useEffect(() => {
+    setData(item);
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className={s["modal-content"]}>
+      <div className={s["modal-header"]}>
+        <div className={s["modal-box-header"]}>
+          <h5>Cập nhật nhóm nhà cung cấp</h5>
+        </div>
+      </div>
+
+      <div className={s["modal-body"]}>
+        <div className={s["column-body"]}>
+          <div className={s["row"]}>
+            <label htmlFor="name" className={s["form-label"]}>
+              Tên
+              <span className="asterisk-icon">*</span>
+            </label>
+            <div className={s["form-textfield"]}>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={(e) =>
+                  setData((prev) => {
+                    return {
+                      ...prev,
+                      name: e.target.value,
+                    };
+                  })
+                }
+                value={data?.name}
+              />
+              <fieldset className="input-field"></fieldset>
+            </div>
+          </div>
+          <div className={s["row"]}>
+            <label htmlFor="sub_id" className={s["form-label"]}>
+              Mã
+            </label>
+            <div className={s["form-textfield"]}>
+              <input
+                type="text"
+                name="sub_id"
+                id="sub_id"
+                value={data.sub_id}
+                onChange={(e) =>
+                  setData((prev) => {
+                    return {
+                      ...prev,
+                      sub_id: e.target.value,
+                    };
+                  })
+                }
+              />
+              <fieldset className="input-field"></fieldset>
+            </div>
+          </div>
+        </div>
+
+        <div className={s["column-body"]}>
+          <div className={s["row"]}>
+            <label htmlFor="note" className={s["form-label"]}>
+              Mô tả
+            </label>
+            <div className={s["form-textfield"]}>
+              <textarea
+                type="text"
+                name="note"
+                id="note"
+                value={data.note}
+                onChange={(e) =>
+                  setData((prev) => {
+                    return {
+                      ...prev,
+                      note: e.target.value,
+                    };
+                  })
+                }
+              />
+              <fieldset className="input-field"></fieldset>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={s["modal-footer"]}>
+        <button className="btn btn-outline-primary" onClick={handleOnClickBack}>
+          <span className="btn__title">Thoát</span>
+        </button>
+        <button className="btn btn-primary" onClick={handleUpdate}>
+          <span className="btn__title">Cập nhật</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateSupplierGroup;
